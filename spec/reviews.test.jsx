209d02c4b66afad5df94b8100a2901691300ee8e
Audree Steinberg <audreesteinberg@gmail.com>
2021-02-26 17:28:56 -0800

    Install jest

---
+++ spec/reviews.test.jsx
@@ -0,0 +1,10 @@
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import App from '../client/src/App';
+
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<App />);
+});
