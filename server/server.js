2ff8d4c962510db4416723d7f21aed1f60c445a9
Marco Garcia <marcogarcia831@gmail.com>
2021-02-27 19:04:19 -0800

    added product id data and passed all data to the components

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const path = require('path');
 const express = require('express');
+const key = require('../config.js')
+const axios = require('axios')
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/data', (req,res)=>{
+  let options = {
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/products',
+    method: 'GET',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': key
+    }
+  }
+  axios(options).then(response => {res.send(response.data) } )
+})
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
