07a0a1afcc5ad51ad97fda3587b0d7f9d1f6d8d7
Marco Garcia <marcogarcia831@gmail.com>
2021-02-28 17:40:11 -0800

    added the data from the api

--- client/src/Components/QuestionsAnswersSect/QuestionsAnswersSect.jsx
+++ client/src/Components/QuestionsAnswersSect/QuestionsAnswersSect.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 
 class QuestionsAnswersSect extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.id,
+      QnAlist:[],
+      QnADB: [],
+      Filter:''
+    };
+    this.changefilter = this.changefilter.bind(this)
+  }
+  componentDidMount(){
+    axios.get('/Q&A/data',{ params: {
+      id: this.state.id
+    }}).then(response => {
+      var array = [];
+      array.push(response.data.results[0]);
+      array.push(response.data.results[1]);
+      this.setState({
+      QnADB: response.data.results,
+      QnAlist: array
+    })})
+  }
+
+  changefilter(){
+    this.setState({
+      Filter: event.target.value
+    })
+
+
   }
 
   render() {
+    console.log(this.state.QnADB)
+    console.log(this.state.QnAlist)
+
     return(
+      <>
       <h1>Questions Answers Section</h1>
+      <form>
+        <input value={this.state.Filter} placeholder={'HAVE A QUESTION? SEARCH FOR ANSWERS...'} onChange={(event)=>{event.preventDefault(); this.changefilter()}} ></input>
+        <button> Q </button>
+      </form>
+      <p>{this.state.id}</p>
+      </>
     )
   }
 }
--- server/server.js
+++ server/server.js
@@ -8,6 +8,18 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
+app.get('/Q&A/data', (req,res)=>{
+  console.log(req.query.id)
+  let options = {
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/qa/questions?product_id=' + req.query.id,
+    method: 'GET',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': key
+    }
+  }
+  axios(options).then(response => {res.send(response.data) } )
+})
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
