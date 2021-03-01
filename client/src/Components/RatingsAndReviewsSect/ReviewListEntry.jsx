06581566c8796381e956580cb6e895692f9ba497
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-01 11:42:11 -0800

    Implement list of reviews that renders title, body, stars, username, and date of reviews. Creates a counter above the list that renders the number of reviews for a given product

--- client/src/Components/RatingsAndReviewsSect/ReviewListEntry.jsx
+++ client/src/Components/RatingsAndReviewsSect/ReviewListEntry.jsx
@@ -3,28 +3,13 @@ import Stars from './Stars.jsx';
 
 let ReviewListEntry = ({review}) => {
   return (
-    <div>
+    <div className="review-list-entry">
       <Stars rating={review.rating}/>
+      <h4 className="review-title">{review.summary}</h4>
+      <span className="name-date">{review.reviewer_name} | {review.date}</span>
+      <h5 className="review-body">{review.body}</h5>
     </div>
   )
 }
-
-// {
-//   "review_id": 147678,
-//   "rating": 5,
-//   "summary": "Id itaque est placeat soluta sit soluta perspiciatis.",
-//   "recommend": true,
-//   "response": null,
-//   "body": "Molestiae laborum accusamus natus necessitatibus cum debitis autem soluta impedit. Maiores consequuntur delectus. Nostrum deserunt aliquid dolores commodi itaque excepturi sunt. Totam excepturi dolorem est. Autem libero est aut occaecati facilis qui corporis id. Soluta dolor vero rerum facere reprehenderit atque.",
-//   "date": "2020-08-05T00:00:00.000Z",
-//   "reviewer_name": "Maggie6",
-//   "helpfulness": 26,
-//   "photos": [
-//       {
-//           "id": 187149,
-//           "url": "https://images.unsplash.com/photo-1542818212-9899bafcb9db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1526&q=80"
-//       }
-//   ]
-// },
 
 export default ReviewListEntry;
