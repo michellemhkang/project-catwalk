7686fb33ead5e0be414452b3e8588c8a538865be
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-02 17:49:43 -0800

    Add summary text area

--- client/src/Components/RatingsAndReviewsSect/WriteYourReview.jsx
+++ client/src/Components/RatingsAndReviewsSect/WriteYourReview.jsx
@@ -6,13 +6,19 @@ class WriteYourReview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      recommend: 'yes'
+      recommend: true,
+      summary: ''
     }
     this.handleRecommendChange = this.handleRecommendChange.bind(this);
+    this.handleSummaryChange = this.handleSummaryChange.bind(this);
   }
 
   handleRecommendChange(e) {
     this.setState({recommend: e.target.value})
+  }
+
+  handleSummaryChange(e) {
+    this.setState({summary: e.target.value})
   }
 
   render() {
@@ -27,26 +33,30 @@ class WriteYourReview extends React.Component {
         <span className={styles.starRow}>
           <p>Do you recommend this product?</p>
           <div onChange={this.handleRecommendChange}>
+              <label>
+                <input
+                  type="radio"
+                  value="true"
+                  checked={this.state.recommend === "true"}
+                  onChange={this.handleChange}
+                />
+                Yes
+            </label>
             <label>
-              <input
-                type="radio"
-                value="true"
-                checked={this.state.recommend === "true"}
-                onChange={this.handleChange}
-              />
-              Yes
-          </label>
-          <label>
-              <input
-                type="radio"
-                value="false"
-                checked={this.state.recommend === "false"}
-                onChange={this.handleChange}
-              />
-              No
-          </label>
+                <input
+                  type="radio"
+                  value="false"
+                  checked={this.state.recommend === "false"}
+                  onChange={this.handleChange}
+                />
+                No
+            </label>
           </div>
-        </span>
+         </span>
+        <div className={styles.reviewInput}>
+            Summary:
+            <input type="text" placeholder="Example: 'Best Purchase Ever!'" value={this.state.summary} onChange={this.handleSummaryChange} />
+        </div>
       </div>
     )
   }
