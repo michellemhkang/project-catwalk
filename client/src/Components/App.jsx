2906539e7494de67ce1c1d4aa255eb4f48e9bf85
Gordon-Chow <Roarekknight@gmail.com>
2021-03-03 16:27:59 -0800

    Add two new states to App for future use

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -10,13 +10,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: 14931
+      id: 14931,
+      productInfo: {},
+      styleInfo: []
     };
     this.changePage = this.changePage.bind(this);
+    this.getProductInfo = this.getProductInfo.bind(this);
+    this.getStyleInfo = this.getStyleInfo.bind(this);
   }
 
   changePage(newid) {
     this.setState({id: newid })
+    // console.log(this.state);
   }
 
   render() {
