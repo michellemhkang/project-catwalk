638d300fd54cd1f6a56834528c28b61f4a5f5b40
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-05 13:36:05 -0800

    Add functionality to ratings to render half stars

--- client/src/Components/RatingsAndReviewsSect/Stars.jsx
+++ client/src/Components/RatingsAndReviewsSect/Stars.jsx
@@ -5,13 +5,61 @@ let Stars = (props) => {
   let rating;
   props.rating ? rating = props.rating : rating = props.averageRating;
 
+  // let renderRatingStars = (rating) => {
+  //   let stars = [];
+  //   for (var i = 0; i < rating; i++) {
+  //     stars.push(<i className="fas fa-star" key={i}></i>);
+  //   }
+  //   for (var j = 0; j < 5 - rating; j++) {
+  //     stars.push(<i className="far fa-star" key={i}></i>)
+  //   }
+  //   return stars;
+  // }
+
+  // let renderRatingStars = (rating) => {
+  //   let stars = [];
+  //   let index = rating;
+  //   while (index > 0) {
+  //     if (index < 1) {
+  //       if (Math.round(index) === 1) {
+  //         stars.push(<i className="fas fa-star-half-alt" key={index}></i>)
+  //       }
+  //     } else {
+  //       stars.push(<i className="fas fa-star" key={index}></i>);
+  //     }
+  //     index--;
+  //   }
+  //   let remaining = 5 - stars.length;
+  //   for (var i = 0; i < remaining; i++) {
+  //     stars.push(<i className="far fa-star" key={i}></i>)
+  //   }
+  //   return stars;
+  // }
+
   let renderRatingStars = (rating) => {
     let stars = [];
-    for (var i = 0; i < rating; i++) {
-      stars.push(<i className="fas fa-star" key={i}></i>);
+    let index = rating;
+    let remainder = index % index;
+
+    while (index > 0) {
+      if (index < 1) {
+        if (index >= 0.5) {
+          stars.push(<i className="fas fa-star-half-alt" key={index}></i>)
+        }
+      } else {
+        stars.push(<i className="fas fa-star" key={index}></i>);
+      }
+      index--;
+    }
+
+    console.log(stars.length)
+    for (var i = stars.length; i < 5; i++) {
+      stars.push(<i className="far fa-star"></i>)
     }
     return stars;
   }
+
+
 
   return(
     <span>
