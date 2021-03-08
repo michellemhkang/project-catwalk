a8c2e3bfd191fac0d5c53e58354ab023a4280bb0
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-07 21:28:33 -0800

    Remove mobile div to replace modal due to bugs in duplicate state

--- client/src/Components/RatingsAndReviewsSect/Modal.jsx
+++ client/src/Components/RatingsAndReviewsSect/Modal.jsx
@@ -5,17 +5,12 @@ const Modal = (props) => {
   return (
     !props.show
       ? null
-      : <><div className={`${styles.row} ${styles.modal}`} onClick={props.handleClose}>
+      : <div className={`${styles.row} ${styles.modal}`} onClick={props.handleClose}>
           <div className={`${styles.modalContent} ${styles.fadeIn}`} onClick={e => e.stopPropagation()}>
               <button className={styles.modalCloseButton} onClick={props.handleClose}><i className={`fas fa-times fa-lg`}></i></button>
               {props.children}
+          </div>
         </div>
-        </div>
-    <div className={`${styles.mobileAddReview}`}>
-          <button className={styles.mobilemodalCloseButton} onClick={props.handleClose}><i className="fas fa-times"></i></button>
-          {props.children}
-    </div>
-    </>
   );
 };
 
