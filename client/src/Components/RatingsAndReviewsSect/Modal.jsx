68cdfecf143ebcaefc5fe9b5914f92eca601ae94
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-07 18:09:11 -0800

    Add transition to button hover

--- client/src/Components/RatingsAndReviewsSect/Modal.jsx
+++ client/src/Components/RatingsAndReviewsSect/Modal.jsx
@@ -5,8 +5,8 @@ const Modal = (props) => {
   return (
     !props.show
       ? null
-      : <div className={`${styles.row} ${styles.modal}`}>
-          <div className={`${styles.modalContent} ${styles.fadeIn}`}>
+      : <div className={`${styles.row} ${styles.modal}`} onClick={props.handleClose}>
+          <div className={`${styles.modalContent} ${styles.fadeIn}`} onClick={e => e.stopPropagation()}>
             {/* <div className={styles.modalActions}>
               <h3 className={`${styles.row} ${styles.center} ${styles.newReviewTitle}`}>WRITE YOUR REVIEW</h3> */}
               <button className={styles.modalCloseButton} onClick={props.handleClose}><i className="fas fa-times"></i></button>
