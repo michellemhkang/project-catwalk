a5847db88f48bf77f80693be71ed19c0dbb49eb4
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-07 19:19:24 -0800

    Create media queries to restructure page for mobile devices. Columns stack on top of each other and content from modal becomes bottom block in column

--- client/src/Components/RatingsAndReviewsSect/Modal.jsx
+++ client/src/Components/RatingsAndReviewsSect/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = (props) => {
   return (
     !props.show
       ? null
-      : <div className={`${styles.row} ${styles.modal}`} onClick={props.handleClose}>
+      : <><div className={`${styles.row} ${styles.modal}`} onClick={props.handleClose}>
           <div className={`${styles.modalContent} ${styles.fadeIn}`} onClick={e => e.stopPropagation()}>
             {/* <div className={styles.modalActions}>
               <h3 className={`${styles.row} ${styles.center} ${styles.newReviewTitle}`}>WRITE YOUR REVIEW</h3> */}
@@ -13,6 +13,13 @@ const Modal = (props) => {
               {props.children}
         </div>
         </div>
+    <div className={`${styles.mobileAddReview}`}>
+        {/* <div className={styles.modalActions}>
+          <h3 className={`${styles.row} ${styles.center} ${styles.newReviewTitle}`}>WRITE YOUR REVIEW</h3> */}
+          <button className={styles.modalCloseButton} onClick={props.handleClose}><i className="fas fa-times"></i></button>
+          {props.children}
+    </div>
+    </>
   );
 };
 
