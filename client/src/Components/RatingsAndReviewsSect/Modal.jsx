46080eaef2bee40476dc34e023cdf44f317722ed
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-07 21:12:18 -0800

    Remove unnecessary comments

--- client/src/Components/RatingsAndReviewsSect/Modal.jsx
+++ client/src/Components/RatingsAndReviewsSect/Modal.jsx
@@ -7,15 +7,11 @@ const Modal = (props) => {
       ? null
       : <><div className={`${styles.row} ${styles.modal}`} onClick={props.handleClose}>
           <div className={`${styles.modalContent} ${styles.fadeIn}`} onClick={e => e.stopPropagation()}>
-            {/* <div className={styles.modalActions}>
-              <h3 className={`${styles.row} ${styles.center} ${styles.newReviewTitle}`}>WRITE YOUR REVIEW</h3> */}
               <button className={styles.modalCloseButton} onClick={props.handleClose}><i className={`fas fa-times fa-lg`}></i></button>
               {props.children}
         </div>
         </div>
     <div className={`${styles.mobileAddReview}`}>
-        {/* <div className={styles.modalActions}>
-          <h3 className={`${styles.row} ${styles.center} ${styles.newReviewTitle}`}>WRITE YOUR REVIEW</h3> */}
           <button className={styles.mobilemodalCloseButton} onClick={props.handleClose}><i className="fas fa-times"></i></button>
           {props.children}
     </div>
