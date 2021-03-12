35f34e813d64d9e5b5a3613cb6f329f38b9d3eeb
Audree Steinberg <audreesteinberg@gmail.com>
2021-03-12 11:50:43 -0800

    Remove unnecessary classname

--- client/src/Components/RatingsAndReviewsSect/List.jsx
+++ client/src/Components/RatingsAndReviewsSect/List.jsx
@@ -4,11 +4,10 @@ import ReviewListEntry from './ReviewListEntry.jsx';
 import styles from './reviews.module.css';
 
 let List = ({reviewList}) => {
-  return (<div className={styles.reviewListCol}>
-    {reviewList.map(review => {
+  return (
+    reviewList.map(review => {
       return <ReviewListEntry review={review} key={review.review_id} />
-    })}
-    </div>
+    })
   );
 };
 
