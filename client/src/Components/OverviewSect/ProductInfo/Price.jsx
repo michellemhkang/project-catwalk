9e7ab1045b23b8fcb5db8744dee8aa5fc0e08c1f
Your Name <michellemhkang@gmail.com>
2021-03-13 13:15:33 -0800

    Clean up Product Info

--- client/src/Components/OverviewSect/ProductInfo/Price.jsx
+++ client/src/Components/OverviewSect/ProductInfo/Price.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styling from './ProductInfo.module.css';
 
-const Price = ({defaultPrice, salePrice}) => {
+const Price = ({ defaultPrice, salePrice }) => {
 
     if (salePrice) {
         return (
             <div>
                 <span className={styling.defaultStrikethrough}>${defaultPrice}</span>
-                <span className={styling.salePrice}>        ${salePrice}</span>
+                <span className={styling.salePrice}>           ${salePrice}</span>
             </div>
         )
     }
