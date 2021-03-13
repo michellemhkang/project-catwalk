a5a653207d40ee9b256edf53aebc6649e0b4eaba
Your Name <michellemhkang@gmail.com>
2021-03-13 14:15:34 -0800

    Clean up Image Gallery

--- client/src/Components/OverviewSect/ImageGallery/Arrow.jsx
+++ client/src/Components/OverviewSect/ImageGallery/Arrow.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styling from './ImageGallery.module.css';
 
-const Arrow = (props) => {
+const Arrow = ({ direction, clickFunction }) => {
+
     return (
         <div className={styling.arrow}>
-            <i className={`fas fa-angle-${props.direction}`} onClick={props.clickFunction} />
+            <i className={`fas fa-angle-${direction}`} onClick={clickFunction} />
         </div>
     )
 }
